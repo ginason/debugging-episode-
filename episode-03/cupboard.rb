d78fb280df9e2457c1ea66d3cb998571de85cbd5
Gina Son <ginasunah33@gmail.com>
2017-07-17 18:38:22 -0400

    complete episode

---
+++ episode-03/cupboard.rb
@@ -0,0 +1,30 @@
+# The Cupboard is where empty glasses are stored.
+# When this is instatiated, it should put some empty
+# glasses onto its shelf if it's working properly.
+
+class Cupboard
+
+  def initialize
+    @shelf = Location.new
+    stock_shelf
+  end
+
+  def shelf
+    @shelf
+  end
+
+  def stock_shelf
+    5.times do
+      @shelf.contents << Glass.new
+    end
+  end
+
+  def find_glass
+    @shelf.contents.find { |obj| obj.class == Glass }
+  end
+
+  def wesley_takes_glasses_for_a_science_project
+    @shelf.contents.size.times { @shelf.contents.pop }
+  end
+
+end
